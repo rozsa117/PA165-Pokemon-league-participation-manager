8696e2ede41d9ae2ce43ec4da02f842bb6e00967
Tibor Zauko <xzauko@fi.muni.cz>
2018-10-10 20:57:59 +0200

    Add Pokemon entity class and DAO. Resolves #5

---
+++ pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/entities/DAOInterface/PokemonDAO.java
@@ -0,0 +1,76 @@
+
+package cz.muni.fi.pa165.pokemon.league.participation.manager.entities.DAOInterface;
+
+import cz.muni.fi.pa165.pokemon.league.participation.manager.common.IDException;
+import cz.muni.fi.pa165.pokemon.league.participation.manager.common.ServiceFailureException;
+import cz.muni.fi.pa165.pokemon.league.participation.manager.common.ValidationException;
+import cz.muni.fi.pa165.pokemon.league.participation.manager.entities.Pokemon;
+import java.util.List;
+
+/**
+ * Interface of data access objects for Pokemon entity class.
+ *
+ * @author Tibor Zauko 433531
+ */
+public interface PokemonDAO {
+
+    /**
+     * Store a new Pokemon.
+     *
+     * @param pokemon Pokemon to store.
+     * @throws IDException when pokemon's ID is not null.
+     * @throws ValidationException when level is outside the closed range [1, 100],
+     *      species or trainer is null or nickname is null or empty.
+     * @throws ServiceFailureException when a database operation fails.
+     * @throws IllegalArgumentException when pokemon is null.
+     */
+    void createPokemon(Pokemon pokemon)
+            throws IDException, ValidationException, ServiceFailureException;
+
+    /**
+     * Update an existing Pokemon.
+     *
+     * @param pokemon Pokemon to update.
+     * @throws IDException when the pokemon's id is null or no such Pokemon exists in the database,
+     *      its trainer's id is null or no such trainer exists in the database or its species' id is null
+     *      or no such species exists in the database.
+     * @throws ValidationException when level is outside the closed range [1, 100],
+     *      species or trainer is null or nickname is null or empty.
+     * @throws ServiceFailureException when a database operation fails.
+     * @throws IllegalArgumentException when pokemon is null.
+     */
+    void updatePokemon(Pokemon pokemon)
+            throws IDException, ValidationException, ServiceFailureException;
+
+    /**
+     * Remove a Pokemon.
+     * 
+     * @param pokemon Pokemon to remove.
+     * @throws IDException when pokemon's id is null or no such Pokemon exists in the database.
+     * @throws ServiceFailureException when a database operation fails.
+     * @throws IllegalArgumentException when pokemon is null.
+     */
+    void deletePokemon(Pokemon pokemon)
+            throws IDException, ServiceFailureException;
+
+    /**
+     * Find a Pokemon based on its id.
+     * 
+     * @param id primary key of the requested Pokemon. 
+     * @return the requested Pokemon, null if no such Pokemon exists.
+     * @throws ServiceFailureException when a database operation fails.
+     * @throws IllegalArgumentException when id is null.
+     */
+    Pokemon findPokemonById(Long id)
+            throws ServiceFailureException;
+
+    /**
+     * Return a list of all Pokemon.
+     * 
+     * @return list of all Pokemon. An empty list will be returned if no Pokemon exist.
+     * @throws ServiceFailureException when a database operation fails.
+     */
+    List<Pokemon> getAllPokemon()
+            throws ServiceFailureException;
+
+}
---
+++ pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/entities/Pokemon.java
@@ -0,0 +1,95 @@
+
+package cz.muni.fi.pa165.pokemon.league.participation.manager.entities;
+
+import java.util.Objects;
+
+/**
+ * Entity class representing a Pokemon caught by a trainer.
+ * 
+ * @author Tibor Zauko 433531
+ */
+public class Pokemon {
+    
+    private Long id;
+    private PokemonSpecies species;
+    private String nickname;
+    private int level;
+    private Trainer trainer;
+
+    public Pokemon() {
+        this(null, null, null, 1, null);
+    }
+
+    public Pokemon(Long id, PokemonSpecies species, String nickname, int level, Trainer trainer) {
+        this.id = id;
+        this.species = species;
+        this.nickname = nickname;
+        this.level = level;
+        this.trainer = trainer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public PokemonSpecies getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(PokemonSpecies species) {
+        this.species = species;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public Trainer getTrainer() {
+        return trainer;
+    }
+
+    public void setTrainer(Trainer trainer) {
+        this.trainer = trainer;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 73 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pokemon other = (Pokemon) obj;
+        return Objects.equals(this.id, other.id);
+    }
+    
+    
+    
+}
