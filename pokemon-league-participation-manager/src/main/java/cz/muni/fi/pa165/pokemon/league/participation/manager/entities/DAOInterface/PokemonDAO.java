df882c677d40c97a269ddb5c440e80b319d433c2
Tibor Zauko <xzauko@fi.muni.cz>
2018-10-12 17:53:34 +0200

    Add JPA annotation, remove unnecessary exceptions from DAO

--- pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/entities/DAOInterface/PokemonDAO.java
+++ pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/entities/DAOInterface/PokemonDAO.java
@@ -1,9 +1,7 @@
 
 package cz.muni.fi.pa165.pokemon.league.participation.manager.entities.DAOInterface;
 
-import cz.muni.fi.pa165.pokemon.league.participation.manager.common.IDException;
 import cz.muni.fi.pa165.pokemon.league.participation.manager.common.ServiceFailureException;
-import cz.muni.fi.pa165.pokemon.league.participation.manager.common.ValidationException;
 import cz.muni.fi.pa165.pokemon.league.participation.manager.entities.Pokemon;
 import java.util.List;
 
@@ -18,42 +16,31 @@ public interface PokemonDAO {
      * Store a new Pokemon.
      *
      * @param pokemon Pokemon to store.
-     * @throws IDException when pokemon's ID is not null, its trainer's id is null
-     *      or no such trainer exists in the database or its species' id is null
-     *      or no such species exists in the database..
-     * @throws ValidationException when level is outside the closed range [1, 100],
-     *      species or trainer is null or nickname is null or empty.
      * @throws ServiceFailureException when a database operation fails.
      * @throws IllegalArgumentException when pokemon is null.
      */
     void createPokemon(Pokemon pokemon)
-            throws IDException, ValidationException, ServiceFailureException;
+            throws ServiceFailureException;
 
     /**
      * Update an existing Pokemon.
      *
      * @param pokemon Pokemon to update.
-     * @throws IDException when the pokemon's id is null or no such Pokemon exists in the database,
-     *      its trainer's id is null or no such trainer exists in the database or its species' id is null
-     *      or no such species exists in the database.
-     * @throws ValidationException when level is outside the closed range [1, 100],
-     *      species or trainer is null or nickname is null or empty.
      * @throws ServiceFailureException when a database operation fails.
      * @throws IllegalArgumentException when pokemon is null.
      */
     void updatePokemon(Pokemon pokemon)
-            throws IDException, ValidationException, ServiceFailureException;
+            throws ServiceFailureException;
 
     /**
      * Remove a Pokemon.
      *
      * @param pokemon Pokemon to remove.
-     * @throws IDException when pokemon's id is null or no such Pokemon exists in the database.
      * @throws ServiceFailureException when a database operation fails.
      * @throws IllegalArgumentException when pokemon is null.
      */
     void deletePokemon(Pokemon pokemon)
-            throws IDException, ServiceFailureException;
+            throws ServiceFailureException;
 
     /**
      * Find a Pokemon based on its id.
