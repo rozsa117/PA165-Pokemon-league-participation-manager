41ab66e31e0cc5a72b1ad757f9970a7e1ef5d3ba
Tamas Rozsa <445653@mail.muni.cz>
2018-10-13 15:48:34 +0200

    Redundant implementations removed form Trainer entity class

--- pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/entities/Trainer.java
+++ pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/entities/Trainer.java
@@ -7,14 +7,10 @@ package cz.muni.fi.pa165.pokemon.league.participation.manager.entities;
 
 import java.time.LocalDate;
 import java.util.Objects;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
