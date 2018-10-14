d125bfdb1256c0ab8119852d14c45077366a036d
Tamas Rozsa <445653@mail.muni.cz>
2018-10-14 23:31:24 +0200

    tag Temporal added

--- pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/entities/Trainer.java
+++ pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/entities/Trainer.java
@@ -12,6 +12,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -34,6 +36,7 @@ public class Trainer {
     private String surname;
     
     @NotNull
+    @Temporal(TemporalType.DATE)
     private LocalDate born;
 
     public Trainer() {
