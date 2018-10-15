3823af52830a1068e7debca7bb604e9fc4ffa235
Tamas Rozsa <445653@mail.muni.cz>
2018-10-15 14:34:13 +0200

    Trainer column name fixed

--- pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/entities/Trainer.java
+++ pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/entities/Trainer.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Column;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -39,6 +40,7 @@ public class Trainer {
     @Temporal(TemporalType.DATE)
     private LocalDate born;
 
+    @Column(name = "trnr_admin")
     private boolean admin;
     
     public Trainer() {
