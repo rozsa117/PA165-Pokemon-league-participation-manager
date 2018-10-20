b0478f3a5466a514b9a8d1ef860189a70dfffe92
Jiří Medveď <medved@volny.cz>
2018-10-20 22:13:42 +0200

    PokemonDAOImpl Skeleton

--- pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/dao/PokemonDAOImpl.java
+++ pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/dao/PokemonDAOImpl.java
@@ -12,35 +12,31 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public class PokemonDAOImpl implements PokemonDAO {
-
-    @PersistenceContext
-    private EntityManager em;
 
     @Override
     public void createPokemon(Pokemon pokemon) throws ServiceFailureException {
-        em.persist(pokemon);
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void updatePokemon(Pokemon pokemon) throws ServiceFailureException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void deletePokemon(Pokemon pokemon) throws ServiceFailureException {
-        em.remove(pokemon);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public Pokemon findPokemonById(Long id) throws ServiceFailureException {
-        return em.find(Pokemon.class, id);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public List<Pokemon> getAllPokemon() throws ServiceFailureException {
-        return em.createQuery("Select p from Pokemon p", Pokemon.class).getResultList();
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
