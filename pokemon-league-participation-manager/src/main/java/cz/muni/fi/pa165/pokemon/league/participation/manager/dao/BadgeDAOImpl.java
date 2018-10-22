72850642913ab650b45585e599daf06718714ce4
Tibor Zauko <xzauko@fi.muni.cz>
2018-10-22 14:14:11 +0200

    Add skeletons for GymDAOImpl and BadgeDAOImpl

---
+++ pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/dao/BadgeDAOImpl.java
@@ -0,0 +1,32 @@
+package cz.muni.fi.pa165.pokemon.league.participation.manager.dao;
+
+import cz.muni.fi.pa165.pokemon.league.participation.manager.entities.Badge;
+import java.util.List;
+
+/**
+ *
+ * @author Tibor Zauko 433531
+ */
+public class BadgeDAOImpl implements BadgeDAO {
+
+    @Override
+    public void createBadge(Badge badge) {
+    }
+
+    @Override
+    public void updateBadge(Badge badge) {
+    }
+
+    @Override
+    public void deleteBadge(Badge badge) {
+    }
+
+    @Override
+    public Badge findBadgeById(Long id) {
+    }
+
+    @Override
+    public List<Badge> getAllBadges() {
+    }
+    
+}
---
+++ pokemon-league-participation-manager/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/dao/GymDAOImpl.java
@@ -0,0 +1,32 @@
+package cz.muni.fi.pa165.pokemon.league.participation.manager.dao;
+
+import cz.muni.fi.pa165.pokemon.league.participation.manager.entities.Gym;
+import java.util.List;
+
+/**
+ *
+ * @author Tibor Zauko 433531
+ */
+public class GymDAOImpl implements GymDAO {
+
+    @Override
+    public void createGym(Gym gym) {
+    }
+
+    @Override
+    public void updateGym(Gym gym) {
+    }
+
+    @Override
+    public void deleteGym(Gym gym) {
+    }
+
+    @Override
+    public Gym findGymById(Long id) {
+    }
+
+    @Override
+    public List<Gym> getAllGyms() {
+    }
+    
+}
