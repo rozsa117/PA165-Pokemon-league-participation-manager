5f6e8fbb0ca5b0d783ab771f78a6ba4b09c7a850
xmokros <456442@mail.muni.cz>
2018-11-09 21:30:02 +0100

    added method into badge facade interface

--- pokemon-api/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/facade/BadgeFacade.java
+++ pokemon-api/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/facade/BadgeFacade.java
@@ -1,4 +1,15 @@
 package cz.muni.fi.pa165.pokemon.league.participation.manager.facade;
 
-public class BadgeFacade {
+import cz.muni.fi.pa165.pokemon.league.participation.manager.dto.BadgeCreateDTO;
+import cz.muni.fi.pa165.pokemon.league.participation.manager.dto.BadgeDTO;
+import cz.muni.fi.pa165.pokemon.league.participation.manager.dto.GymDTO;
+import cz.muni.fi.pa165.pokemon.league.participation.manager.dto.TrainerDTO;
+import cz.muni.fi.pa165.pokemon.league.participation.manager.enums.ChallengeStatus;
+
+public interface BadgeFacade {
+    Long createBadge(BadgeCreateDTO badge);
+    BadgeDTO findBadgeById(Long id);
+    GymDTO getGymOfBadge(Long id);
+    TrainerDTO getTrainerOfBadge(Long id);
+    void updateStatusOnBadge(Long id, ChallengeStatus status);
 }
