43964ecf60d4b5369bd1f5a72ae91112b25afe82
Tibor Zauko <xzauko@fi.muni.cz>
2018-11-14 12:53:13 +0100

    Add javadoc to facade interfaces, missing thrown exceptions.

--- pokemon-api/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/facade/PokemonFacade.java
+++ pokemon-api/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/facade/PokemonFacade.java
@@ -16,19 +16,62 @@ import java.util.List;
  */
 public interface PokemonFacade {
     
+    /**
+     * Create a Pokemon.
+     * 
+     * @param pokemon Specification of Pokemon to create.
+     * @return ID of the created Pokemon.
+     */
     Long createPokemon(PokemonCreateDTO pokemon);
     
+    /**
+     * Finds Pokemon with the specified ID.
+     * 
+     * @param id ID of the Pokemon to find.
+     * @return Found Pokemon, null if no such Pokemon exists.
+     */
     PokemonDTO findPokemonById(Long id);
     
+    /**
+     * Return all Pokemon of a trainer.
+     * 
+     * @param trainerId ID of the trainer whose Pokemon shall be returned.
+     * @return List of all of the trainer's Pokemon.
+     */
     List<PokemonDTO> getPokemonOfTrainer(Long trainerId);
     
-    void releasePokemon(ReleasePokemonDTO id);
+    /**
+     * Release a Pokemon (remove it).
+     * 
+     * @param releasedPokemon Specification of Pokemon to release. 
+     */
+    void releasePokemon(ReleasePokemonDTO releasedPokemon);
     
+    /**
+     * Rename a Pokemon (change it's nickname).
+     * 
+     * @param newNickname Specification of the new nickname.
+     */
     void renamePokemon(RenamePokemonDTO newNickname);
     
-    void levelUpPokemon(LevelUpPokemonDTO levelIncrease);
+    /**
+     * Increase the level of a Pokemon to given level.
+     * 
+     * @param newLevel Specification of the new level. 
+     */
+    void levelUpPokemon(LevelUpPokemonDTO newLevel);
     
+    /**
+     * Change the Pokemon's species to one of it's current species' evolutions.
+     * 
+     * @param chosenEvolution Specification of the chosen evolution.
+     */
     void evolvePokemon(EvolvePokemonDTO chosenEvolution);
     
+    /**
+     * Gift Pokemon to another trainer (make the Pokemon belong to the other trainer).
+     * 
+     * @param gift Specification of gifted Pokemon and which trainer it is gifted to.
+     */
     void giftPokemon(GiftPokemonDTO gift);
 }
--- pokemon-api/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/facade/PokemonSpeciesFacade.java
+++ pokemon-api/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/facade/PokemonSpeciesFacade.java
@@ -13,16 +13,58 @@ import java.util.List;
  */
 public interface PokemonSpeciesFacade {
     
+    /**
+     * Creates Pokemon species as described in species.
+     * 
+     * @param species Species description.
+     * @return ID of the created species.
+     */
     Long createPokemonSpecies(PokemonSpeciesCreateDTO species);
     
+    /**
+     * Finds Pokemon species with the specified ID.
+     * 
+     * @param id ID of the species to find.
+     * @return Found species, null if no such species exists.
+     */
     PokemonSpeciesDTO findPokemonSpeciesById(Long id);
     
+    /**
+     * Return a list of all Pokemon species.
+     * 
+     * @return List of all Pokemon species.
+     */
     List<PokemonSpeciesDTO> getAllPokemonSpecies();
     
+    /**
+     * Return a list of all Pokemon directly evolving from given species.
+     * 
+     * @param speciesId ID of the species whose evolutions shall be returned.
+     * @return List of all species evolving from given species, if speciesId is
+     * null, then list of all species which don't have preevolutions (NOTE: test
+     * if null case is viable).
+     */
     List<PokemonSpeciesDTO> getAllEvolutionsOfPokemonSpecies(Long speciesId);
     
+    /**
+     * Change typing of a Pokemon species.
+     * 
+     * @param newTyping Specification of new typing.
+     */
     void changeTyping(ChangeTypingDTO newTyping);
     
+    /**
+     * Change preevolution of a species.
+     * 
+     * @param newPreevolution Specification of new preevolution.
+     */
     void changePreevolution(ChangePreevolutionDTO newPreevolution);
+    
+    /**
+     * Remove a Pokemon species.
+     * 
+     * @param speciesId ID of the species to remove.
+     */
+    void removePokemonSpecies(Long speciesId);
     
 }
