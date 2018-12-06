d184c6fc62d2e12dd7712a84d5cdbe2bb107360d
Jiri Medved <medved@volny.cz>
2018-12-06 14:50:45 +0100

    Mock PokemonSpeciesFacade

--- pokemon-rest/src/test/java/cz/muni/fi/pa165/pokemon/league/participation/manager/rest/controllers/AbstractTest.java
+++ pokemon-rest/src/test/java/cz/muni/fi/pa165/pokemon/league/participation/manager/rest/controllers/AbstractTest.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.pokemon.league.participation.manager.rest.controllers;
-
 
 import java.io.IOException;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -20,27 +13,33 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import cz.muni.fi.pa165.pokemon.league.participation.manager.RootWebContext;
+import com.github.dozermapper.core.inject.Inject;
+import cz.muni.fi.pa165.pokemon.league.participation.manager.TestContext;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = RootWebContext.class)
+@SpringBootTest(classes = TestContext.class)
 @WebAppConfiguration
 public abstract class AbstractTest {
-   protected MockMvc mvc;
-   @Autowired
-   WebApplicationContext webApplicationContext;
 
-   protected void setUp() {
-      mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-   }
-   protected String mapToJson(Object obj) throws JsonProcessingException {
-      ObjectMapper objectMapper = new ObjectMapper();
-      return objectMapper.writeValueAsString(obj);
-   }
-   protected <T> T mapFromJson(String json, Class<T> clazz)
-      throws JsonParseException, JsonMappingException, IOException {
-      
-      ObjectMapper objectMapper = new ObjectMapper();
-      return objectMapper.readValue(json, clazz);
-   }
+    protected MockMvc mvc;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    protected void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    protected String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+
+    protected <T> T mapFromJson(String json, Class<T> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
+    }
 }
--- pokemon-rest/src/test/java/cz/muni/fi/pa165/pokemon/league/participation/manager/rest/controllers/PokemonSpeciesControllerTest.java
+++ pokemon-rest/src/test/java/cz/muni/fi/pa165/pokemon/league/participation/manager/rest/controllers/PokemonSpeciesControllerTest.java
@@ -15,26 +15,72 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import cz.muni.fi.pa165.pokemon.league.participation.manager.dto.PokemonSpeciesDTO;
+import org.mockito.Mock;
+
+import static cz.muni.fi.pa165.pokemon.league.participation.manager.ApiUris.*;
+import cz.muni.fi.pa165.pokemon.league.participation.manager.enums.PokemonType;
+import cz.muni.fi.pa165.pokemon.league.participation.manager.facade.PokemonSpeciesFacade;
+import java.util.Arrays;
+import static org.mockito.Mockito.when;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 public class PokemonSpeciesControllerTest extends AbstractTest {
-   @Override
-   @Before
-   public void setUp() {
-      super.setUp();
-   }
-   @Test
-   public void getProductsList() throws Exception {
-      String uri = "/pokemonSpecies";
-      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
-      
-      int status = mvcResult.getResponse().getStatus();
-      assertEquals(200, status);
-      String content = mvcResult.getResponse().getContentAsString();
-      PokemonSpeciesDTO[] pokemonSpeciesList = super.mapFromJson(content, PokemonSpeciesDTO[].class);
-      assertTrue(pokemonSpeciesList.length > 0);
-   }
-   /*
+
+    @MockBean
+    private PokemonSpeciesFacade pokemonSpeciesFacade;
+
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Test
+    public void getPokemonSpeciesList() throws Exception {
+        PokemonSpeciesDTO firstPs = new PokemonSpeciesDTO();
+        firstPs.setId(1l);
+        firstPs.setPrimaryType(PokemonType.FIRE);
+        firstPs.setSpeciesName("First");
+
+        PokemonSpeciesDTO secondPs = new PokemonSpeciesDTO();
+        secondPs.setId(2l);
+        secondPs.setPrimaryType(PokemonType.BUG);
+        secondPs.setSpeciesName("Second");
+
+        when(pokemonSpeciesFacade.getAllPokemonSpecies()).thenReturn(Arrays.asList(firstPs, secondPs));
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(POKEMON_SPECIES_URI)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        PokemonSpeciesDTO[] pokemonSpeciesList = super.mapFromJson(content, PokemonSpeciesDTO[].class);
+        assertTrue(pokemonSpeciesList.length == 2);
+    }
+
+    
+    @Test
+    public void getPokemonSpecies() throws Exception {
+
+        PokemonSpeciesDTO ps = new PokemonSpeciesDTO();
+        ps.setId(1l);
+        ps.setPrimaryType(PokemonType.FIRE);
+        ps.setSpeciesName("First");
+        
+        when(pokemonSpeciesFacade.findPokemonSpeciesById(ps.getId())).thenReturn(ps);
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(POKEMON_SPECIES_URI + "/1")
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        PokemonSpeciesDTO psResult = super.mapFromJson(content, PokemonSpeciesDTO.class);
+        assertEquals( psResult, ps);
+    }
+    
+    /*
    @Test
    public void createProduct() throws Exception {
       String uri = "/products";
@@ -75,5 +121,5 @@ public class PokemonSpeciesControllerTest extends AbstractTest {
       String content = mvcResult.getResponse().getContentAsString();
       assertEquals(content, "Product is deleted successsfully");
    }
-*/
+     */
 }
