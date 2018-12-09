462f71b4d930c6308accc82130573d9e4cf6c491
Jiri Medved <medved@volny.cz>
2018-12-09 19:07:11 +0100

    NoSuchEntityException handling

--- pokemon-service/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/facade/PokemonSpeciesFacadeImpl.java
+++ pokemon-service/src/main/java/cz/muni/fi/pa165/pokemon/league/participation/manager/facade/PokemonSpeciesFacadeImpl.java
@@ -60,7 +60,7 @@ public class PokemonSpeciesFacadeImpl implements PokemonSpeciesFacade {
         return beanMappingService.mapTo(
                 pokemonSpeciesService.getAllEvolutionsOfPokemonSpecies(
                         speciesId == null ? null : pokemonSpeciesService.findPokemonSpeciesById(speciesId)),
-                 PokemonSpeciesDTO.class);
+                PokemonSpeciesDTO.class);
     }
 
     @Override
@@ -73,15 +73,17 @@ public class PokemonSpeciesFacadeImpl implements PokemonSpeciesFacade {
     public void changePreevolution(ChangePreevolutionDTO newPreevolution)
             throws EvolutionChainTooLongException, CircularEvolutionChainException, NoSuchEntityException {
         PokemonSpecies species = getNonNullSpecies(newPreevolution.getSpeciesId());
-            pokemonSpeciesService.changePreevolution(
-                    species,
-                    pokemonSpeciesService.findPokemonSpeciesById(newPreevolution.getPreevolutionId())
-            );
+        Long newPreEvolutionId = newPreevolution.getPreevolutionId();
+        pokemonSpeciesService.changePreevolution(
+                species,
+                (newPreEvolutionId != null ? getNonNullSpecies(newPreEvolutionId) : null)
+        );
     }
 
     @Override
-    public void removePokemonSpecies(@NotNull Long speciesId) throws EntityIsUsedException {
-        PokemonSpecies species = pokemonSpeciesService.findPokemonSpeciesById(speciesId);
+    public void removePokemonSpecies(@NotNull Long speciesId)
+            throws EntityIsUsedException, NoSuchEntityException {
+        PokemonSpecies species = getNonNullSpecies(speciesId);
         if (species != null) {
             pokemonSpeciesService.remove(species);
         }
